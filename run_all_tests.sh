cf8721a77ff3ced2327d01ee13c44202fc4e67bf
Matt Clark <36630558+mattclark-zerogravit@users.noreply.github.com>
2023-01-09 15:01:13 +0000

    API-5500: updated scalafmt dependency, config and added to run all tests

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sbt clean compile coverage scalastyle test it:test coverageReport
+sbt clean scalafmtAll scalafix test:scalafmtAll  test:scalafix it:test::scalafmtAll it:test::scalafix coverage test it:test coverageReport
